e8f4ad846a91fbf04cd5133581c4e17f63fc9930
yasithWimukthi <it19966922@my.sliit.lk>
2020-12-23 23:39:32 +0530

    Edit Form component

--- src/components/home/Form.js
+++ src/components/home/Form.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import useInput from '../../customhook/useInput';
+import {addNote} from '../../store/actions/noteAction';
+import {useDispatch} from 'react-redux';
 
 const Form = () => {
 
     const [title,bindTitle,resetTitle] = useInput();
     const [content,bindContent,resetContent] = useInput();
+    const dispatch = useDispatch();
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        dispatch(addNote({title,content}));
         resetTitle();
         resetContent();
     }
