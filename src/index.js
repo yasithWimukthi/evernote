3d538b34ed14ebb00b2dbc3b70b149ba8a4b6984
yasithWimukthi <it19966922@my.sliit.lk>
2020-12-23 23:23:50 +0530

    Link Firebase

--- src/index.js
+++ src/index.js
@@ -3,11 +3,35 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore,applyMiddleware,compose} from "redux";
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import {ReactReduxFirebaseProvider,getFirebase} from 'react-redux-firebase';
+import {createFirestoreInstance,getFirestore, reduxFirestore} from 'redux-firestore';
+import firebase from 'firebase/app';
+import rootReducer from './store/reducers/rootReducer';
+import fbconfig from './config/fbconfig';
+
+const store = createStore(rootReducer,compose(
+  applyMiddleware (thunk.withExtraArgument({getFirebase,getFirestore})),
+  reduxFirestore(fbconfig))
+)
+
+const rffProps = {
+  firebase,
+  config : fbconfig,
+  dispatch: store.dispatch,
+  createFirestoreInstance
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <ReactReduxFirebaseProvider {...rffProps}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+  </ReactReduxFirebaseProvider>
+  </Provider>,
   document.getElementById('root')
 );
 
